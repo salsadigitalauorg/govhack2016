003ab68994bc2a6fa7aa7222d2f313e532bf7774
Kurt Foster <kurt@salsadigital.com.au>
2016-07-30 11:00:44 +1000

    Readding the acquit info to settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -213,9 +213,9 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'govhack',
       'username' => 'root',
@@ -598,3 +598,11 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash sign to enable.
  */
 # $conf['theme_debug'] = TRUE;
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/govhack/govhack-settings.inc');
+}
